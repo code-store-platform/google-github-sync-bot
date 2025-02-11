b3086bce3e229c18f88daa748778d9955f6c18dd
Arthur Murauskas <amurauskas@code.store>
2025-02-11 17:57:50 +0100

    Getting list of users from google workspace

--- index.ts
+++ index.ts
@@ -1,1 +1,68 @@
-console.log("Hello via Bun!");
+import {google } from 'googleapis';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+type GoogleUser = {
+  primaryEmail?: string | null;
+  customSchemas?: {
+    '3rd-party_tools'?: {
+      GitHub_Username?: string;
+    } | null;
+  } | null;
+}
+
+class WorkspaceGitHubSync {
+  private readonly auth;
+
+  constructor() {
+    this.auth = new google.auth.GoogleAuth({
+      keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+      scopes: ['https://www.googleapis.com/auth/admin.directory.user.readonly'],
+      clientOptions: {
+        subject: process.env.GOOGLE_ADMIN_EMAIL, // impersonate the admin user
+      }
+    });
+  }
+
+  async syncMembers() {
+    try {
+      const workspaceUsers = await this.getWorkspaceUsers();
+
+      // get all users with GitHub usernames
+      const workspaceGitHubUsers = new Set(
+        workspaceUsers
+          .filter(user => !!user.customSchemas && user.customSchemas["3rd-party_tools"]?.GitHub_Username)
+          .map(user => user.customSchemas!["3rd-party_tools"]!.GitHub_Username!)
+      );
+    }
+    catch (e) {
+      console.error('Error during synchronization', e);
+      throw e;
+    }
+  }
+
+  async getWorkspaceUsers(): Promise<GoogleUser[]> {
+    const directory = google.admin({ version: 'directory_v1', auth: this.auth });
+
+    try {
+      const response = await directory.users.list({
+        customer: 'my_customer',
+        projection: 'full',
+        viewType: 'domain_public',
+      });
+
+      return response.data.users || [];
+    } catch (e) {
+      console.error('Error fetching Google Workspace users', e);
+      throw e;
+    }
+  }
+}
+
+async function main() {
+  const sync = new WorkspaceGitHubSync();
+  await sync.syncMembers();
+}
+
+main().catch(console.error);
